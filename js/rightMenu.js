bd91eb62a538a07c8c5d320999b6894f132fd9c7
lazyDog <1159002599@qq.com>
2023-02-01 05:52:47 +0800

    Site updated: 2023-02-01 05:52:45

--- js/rightMenu.js
+++ js/rightMenu.js
@@ -1,3 +1,53 @@
+function setMask(){//设置遮罩层
+    if(document.getElementsByClassName("rmMask")[0]!=undefined){
+        return document.getElementsByClassName("rmMask")[0];
+    }
+    mask = document.createElement('div');
+    mask.className = "rmMask";
+    mask.style.width = window.innerWidth + 'px';
+    mask.style.height = window.innerHeight + 'px';
+    mask.style.background = '#fff';
+    mask.style.opacity = '.0';
+    mask.style.position = 'fixed';
+    mask.style.top = '0';
+    mask.style.left = '0';
+    mask.style.zIndex = 998;
+    document.body.appendChild(mask);
+    document.getElementById("rightMenu").style.zIndex=19198;
+    return mask;
+}
+
+function insertAtCursor(myField, myValue) {
+
+    //IE 浏览器
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+        sel.select();
+    }
+
+    //FireFox、Chrome等
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+
+        // 保存滚动条
+        var restoreTop = myField.scrollTop;
+        myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
+
+        if (restoreTop > 0) {
+            myField.scrollTop = restoreTop;
+        }
+
+        myField.focus();
+        myField.selectionStart = startPos + myValue.length;
+        myField.selectionEnd = startPos + myValue.length;
+    } else {
+        myField.value += myValue;
+        myField.focus();
+    }
+}
 var rm = {};
 rm.showRightMenu = function (isTrue, x = 0, y = 0) {
     let $rightMenu = $('#rightMenu');
@@ -16,6 +66,25 @@ rm.reloadrmSize = function () {
     rmWidth = $("#rightMenu").width();
     rmHeight = $("#rightMenu").height()
 };
+//复制选中文字
+rm.copySelect = function(){
+    document.execCommand('Copy',false,null);
+    //这里可以写点东西提示一下 已复制
+}
+rm.copyWordsLink = function () {
+    let url = window.location.href
+    let txa = document.createElement("textarea");
+    txa.value = url;
+    document.body.appendChild(txa)
+    txa.select();
+    document.execCommand("Copy");
+    document.body.removeChild(txa);
+    Snackbar.show({
+        text: '链接复制成功！快去分享吧！',
+        pos: 'top-right',
+        showAction: false
+    });
+}
 window.oncontextmenu = function (event) {
     if (document.body.clientWidth > 768) {
         let pageX = event.clientX + 10;	
@@ -187,3 +256,170 @@ $("#rightmenu-mask").contextmenu(function () {
     removeRightMenu();
     return false;
 });
+function popupMenu() {
+    //window.oncontextmenu=function(){return false;}
+    window.oncontextmenu = function (event) {
+        if(event.ctrlKey)return true;
+        
+        $('.rightMenu-group.hide').hide();
+        if (document.getSelection().toString()) {
+            $('#menu-text').show();
+        }
+        if (document.getElementById('post')) {
+            $('#menu-post').show();
+        } else {
+            if (document.getElementById('page')) {
+                $('#menu-post').show();
+            }
+        }
+        var el = window.document.body;
+        el = event.target;
+        var a=/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\*\+,;=.]+$/
+        if (a.test(window.getSelection().toString())&&el.tagName!="A"){
+            $('#menu-too').show()
+        }
+        if (el.tagName == 'A') {
+            $('#menu-to').show()
+            rm.open = function () {
+                if(el.href.indexOf("http://")==-1&&el.href.indexOf("https://")==-1||el.href.indexOf("yisous.xyz")!=-1){
+                    pjax.loadUrl(el.href)
+                }
+                else{
+                    location.href = el.href
+                }
+            }
+            rm.openWithNewTab = function () {
+                window.open(el.href);
+                // window.location.reload();
+            }
+            rm.copyLink = function () {
+                let url = el.href
+                let txa = document.createElement("textarea");
+                txa.value = url;
+                document.body.appendChild(txa)
+                txa.select();
+                document.execCommand("Copy");
+                document.body.removeChild(txa);
+            }
+        }
+        if (el.tagName == 'IMG') {
+            $('#menu-img').show()
+            rm.openWithNewTab = function () {
+                window.open(el.src);
+                // window.location.reload();
+            }
+            rm.click = function () {
+                el.click()
+            }
+            rm.copyLink = function () {
+                let url = el.src
+                let txa = document.createElement("textarea");
+                txa.value = url;
+                document.body.appendChild(txa)
+                txa.select();
+                document.execCommand("Copy");
+                document.body.removeChild(txa);
+            }
+            rm.saveAs=function(){
+                var a = document.createElement('a');
+                var url = el.src;
+                var filename = url.split("/")[-1];
+                a.href = url;
+                a.download = filename;
+                a.click();
+                window.URL.revokeObjectURL(url);
+            }
+        } else if (el.tagName == "TEXTAREA" || el.tagName == "INPUT") {
+            $('#menu-paste').show();
+            // rm.paste=function(){
+            //     input.addEventListener('paste', async event => {
+            //         event.preventDefault();
+            //         const text = await navigator.clipboard.readText();
+            //         el.value+=text;
+            //       });
+            // }
+            rm.paste = function () {
+                navigator.permissions
+                    .query({
+                        name: 'clipboard-read'
+                    })
+                    .then(result => {
+                        if (result.state == 'granted' || result.state == 'prompt') {
+                            //读取剪贴板
+                            navigator.clipboard.readText().then(text => {
+                                console.log(text)
+                                insertAtCursor(el, text)
+                            })
+                        } else {
+                            Snackbar.show({
+                                text: '请允许读取剪贴板！',
+                                pos: 'top-center',
+                                showAction: false,
+                            })
+                        }
+                    })
+            }
+        }
+        let pageX = event.clientX + 10;
+        let pageY = event.clientY;
+        let rmWidth = $('#rightMenu').width();
+        let rmHeight = $('#rightMenu').height();
+        if (pageX + rmWidth > window.innerWidth) {
+            pageX -= rmWidth + 10;
+        }
+        if (pageY + rmHeight > window.innerHeight) {
+            pageY -= pageY + rmHeight - window.innerHeight;
+        }
+        mask=setMask();
+        window.onscroll=()=>{
+            rm.showRightMenu(false);
+            window.onscroll=()=>{}
+            document.body.removeChild(mask);
+        }
+        $(".rightMenu-item").click(()=>{
+            document.body.removeChild(mask);
+        })
+        $(window).resize(()=>{
+            rm.showRightMenu(false);
+            document.body.removeChild(mask);
+        })
+        mask.onclick=()=>{
+            document.body.removeChild(mask);
+        }
+        rm.showRightMenu(true, pageY, pageX);
+        return false;
+    };
+
+    window.addEventListener('click', function () {
+        rm.showRightMenu(false);
+    });
+}
+if (!(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    popupMenu()
+}
+const box = document.documentElement
+
+function addLongtabListener(target, callback) {
+    let timer = 0 // 初始化timer
+
+    target.ontouchstart = () => {
+        timer = 0 // 重置timer
+        timer = setTimeout(() => {
+            callback();
+            timer = 0
+        }, 380) // 超时器能成功执行，说明是长按
+    }
+
+    target.ontouchmove = () => {
+        clearTimeout(timer) // 如果来到这里，说明是滑动
+        timer = 0
+    }
+
+    target.ontouchend = () => { // 到这里如果timer有值，说明此触摸时间不足380ms，是点击
+        if (timer) {
+            clearTimeout(timer)
+        }
+    }
+}
+
+addLongtabListener(box, popupMenu)
